b2eae99ba1c3462ef7f53add654a9b9aeee19427
Sergio Garcia <38561120+sergargar@users.noreply.github.com>
2023-02-07 11:29:39 +0100

    chore(security hub): improve securityhub_enabled check logic (#1851)
    Co-authored-by: sergargar <sergargar@users.noreply.github.com>

--- prowler/providers/aws/services/securityhub/securityhub_service.py
+++ prowler/providers/aws/services/securityhub/securityhub_service.py
@@ -1,5 +1,7 @@
 import threading
-from dataclasses import dataclass
+
+from botocore.client import ClientError
+from pydantic import BaseModel
 
 from prowler.lib.logger import logger
 from prowler.providers.aws.aws_provider import generate_regional_clients
@@ -30,74 +32,64 @@ class SecurityHub:
     def __describe_hub__(self, regional_client):
         logger.info("SecurityHub - Describing Hub...")
         try:
-            get_enabled_standards_paginator = regional_client.get_paginator(
-                "get_enabled_standards"
-            )
-            standards = ""
-            for page in get_enabled_standards_paginator.paginate():
-                for standard in page["StandardsSubscriptions"]:
-                    standards += f" {standard['StandardsArn'].split('/')[1]}"
-            # Security Hub is not enabled in region
-            if standards == "":
-                self.securityhubs.append(
-                    SecurityHubHub(
-                        "",
-                        "Security Hub",
-                        "NOT_AVAILABLE",
-                        "",
-                        regional_client.region,
+            # Check if SecurityHub is active
+            try:
+                hub_arn = regional_client.describe_hub()["HubArn"]
+            except ClientError as e:
+                # Check if Account is subscribed to Security Hub
+                if e.response["Error"]["Code"] == "InvalidAccessException":
+                    self.securityhubs.append(
+                        SecurityHubHub(
+                            arn="",
+                            id="Security Hub",
+                            status="NOT_AVAILABLE",
+                            standards="",
+                            integrations="",
+                            region=regional_client.region,
+                        )
+                    )
+            else:
+                hub_id = hub_arn.split("/")[1]
+                get_enabled_standards_paginator = regional_client.get_paginator(
+                    "get_enabled_standards"
+                )
+                standards = ""
+                for page in get_enabled_standards_paginator.paginate():
+                    for standard in page["StandardsSubscriptions"]:
+                        standards += f"{standard['StandardsArn'].split('/')[1]} "
+                list_enabled_products_for_import_paginator = (
+                    regional_client.get_paginator(
+                        "list_enabled_products_for_import"
                     )
                 )
-            else:
-                # SecurityHub is active so get HubArn
-                hub_arn = regional_client.describe_hub()["HubArn"]
-                hub_id = hub_arn.split("/")[1]
+                integrations = ""
+                for page in list_enabled_products_for_import_paginator.paginate():
+                    for integration in page["ProductSubscriptions"]:
+                        if (
+                            "/aws/securityhub" not in integration
+                        ):  # ignore Security Hub integration with itself
+                            integrations += f"{integration.split('/')[-1]} "
                 self.securityhubs.append(
                     SecurityHubHub(
-                        hub_arn,
-                        hub_id,
-                        "ACTIVE",
-                        standards,
-                        regional_client.region,
+                        arn=hub_arn,
+                        id=hub_id,
+                        status="ACTIVE",
+                        standards=standards,
+                        integrations=integrations,
+                        region=regional_client.region,
                     )
                 )
 
         except Exception as error:
-            # Check if Account is subscribed to Security Hub
-            if "InvalidAccessException" in str(error):
-                self.securityhubs.append(
-                    SecurityHubHub(
-                        "",
-                        "Security Hub",
-                        "NOT_AVAILABLE",
-                        "",
-                        regional_client.region,
-                    )
-                )
-            else:
-                logger.error(
-                    f"{regional_client.region} -- {error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
-                )
+            logger.error(
+                f"{regional_client.region} -- {error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
+            )
 
 
-@dataclass
-class SecurityHubHub:
+class SecurityHubHub(BaseModel):
     arn: str
     id: str
     status: str
     standards: str
+    integrations: str
     region: str
-
-    def __init__(
-        self,
-        arn,
-        id,
-        status,
-        standards,
-        region,
-    ):
-        self.arn = arn
-        self.id = id
-        self.status = status
-        self.standards = standards
-        self.region = region
